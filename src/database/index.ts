9e0cc3866048802d48c8a81c3a707e9caa6a9f98
Ailén <128065511+ailenpaez@users.noreply.github.com>
2024-10-23 04:26:20 -0300

    Conectando db, paso 1.

---
+++ src/database/index.ts
@@ -0,0 +1,15 @@
+import { Sequelize, DataTypes } from "sequelize";
+
+const sequelize = new Sequelize(process.env.DB_URI);
+
+(async () => {
+    try {
+        await sequelize.authenticate();
+        console.log("CONNECTION_HAS_BEEN_ESTABLISHED_SUCCESSFULLY :)");
+    } catch (error) {
+        console.error("UNABLE_TO_CONNECT_TO_DATABASE :(", error);
+    }
+})();
+
+export { DataTypes };
+export default sequelize;
